9348583cc154aba0b7713748d326bce7b7fdeb88
Thirumalai <84331514+ThiruMalai31@users.noreply.github.com>
2024-10-01 12:26:29 +0530

    Enum.java

--- JavaAssignment/src/Enum.java
+++ JavaAssignment/src/Enum.java
@@ -3,22 +3,10 @@ import java.util.Scanner;
 public class Enum {
     enum Day{
         SUNDAY,MONDAY,TUESDAY,WEDNESDAY,THURSDAY,FRIDAY,SATURDAY;
-
         public static Day getDay(int dayIndex){
-            switch (dayIndex){
-                case 0: return SUNDAY;
-                case 1: return MONDAY;
-                case 2: return TUESDAY;
-                case 3: return WEDNESDAY;
-                case 4: return THURSDAY;
-                case 5: return FRIDAY;
-                case 6: return SATURDAY;
-                default: return null;
-            }
-
+                return Day.values()[dayIndex];
         }
     }
-
     public static void main(String[] args) {
         System.out.println("Enter Any Index from (1-7)");
         Scanner scanner=new Scanner(System.in);
@@ -31,9 +19,6 @@ public class Enum {
         else{
             System.out.println("Enter Valid Date Index");
         }
-
-
-
     }
 
 }
